2c8bfafae5776438ba6383b241fcda2f7fac4ed9
abid83 <47189964+abid83@users.noreply.github.com>
2024-01-21 18:30:20 +0500

    Update App.js v3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js to Amplify</code> and save to reload.
+          Edit <code>src/App.js to Amplify on aws </code> and save to reload.
         </p>
         <a
           className="App-link"
